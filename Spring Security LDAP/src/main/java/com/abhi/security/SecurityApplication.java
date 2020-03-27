49f9b833e35968efdb4764f5085cdde075e5e903
saundab <abhi.saund@gmail.com>
2020-03-28 02:54:02 +0530

    Spring security LDAP removing unwanted code

--- Spring Security LDAP/src/main/java/com/abhi/security/SecurityApplication.java
+++ Spring Security LDAP/src/main/java/com/abhi/security/SecurityApplication.java
@@ -2,12 +2,8 @@ package com.abhi.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import com.abhi.security.repository.UserRepository;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackageClasses=UserRepository.class)
 public class SecurityApplication {
 
 	public static void main(String[] args) {
