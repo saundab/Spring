e8db89eb968e696eb4769cacf9a730aafd0a6978
saundab <abhi.saund@gmail.com>
2020-04-02 20:55:12 +0530

    Spring security OAuth: working code

--- Spring Security OAuth/src/main/java/com/abhi/security/SecurityApplication.java
+++ Spring Security OAuth/src/main/java/com/abhi/security/SecurityApplication.java
@@ -1,11 +1,12 @@
 package com.abhi.security;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication
-@EnableJpaRepositories
+@EnableOAuth2Sso	//it looks into application properties to see whom to call for authentication
 public class SecurityApplication {
 
 	public static void main(String[] args) {
--- Spring Security OAuth/src/main/java/com/abhi/security/controller/HomeController.java
+++ Spring Security OAuth/src/main/java/com/abhi/security/controller/HomeController.java
@@ -1,30 +1,11 @@
 package com.abhi.security.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.abhi.security.dto.AuthenticationRequest;
-import com.abhi.security.dto.AuthenticationResponse;
-import com.abhi.security.service.MyUserDetailsService;
-import com.abhi.security.util.JwtUtil;
 
 @RestController
 public class HomeController {
 
-	@Autowired
-	private AuthenticationManager authManager;
-	
-	@Autowired
-	private MyUserDetailsService userService;
-	
 	@GetMapping("/managers")
 	public String managers() {
 		return "Hello manager !";
@@ -35,17 +16,4 @@ public class HomeController {
 		return "Hello developer!";
 	}
 	
-	@PostMapping("/authenticate")
-	public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) throws Exception{
-		AuthenticationResponse resp =null;
-		
-		//uses the userName service configured in securityConfiguration
-		Authentication auth =  authManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUserName(), request.getPassword()));
-		
-		String jwt = JwtUtil.generateToken((UserDetails)auth.getPrincipal());
-		resp = new AuthenticationResponse(jwt);
-		
-		
-		return ResponseEntity.ok(resp);
-	}
 }
